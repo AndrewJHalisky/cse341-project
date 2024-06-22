51333390102bd9cf65e5de53480a25a143d5e9e9
AndrewJHalisky <ajhalisky@gmail.com>
2024-06-22 16:12:36 -0400

    Changed http https and modified json

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ const userController = require('../controllers/users')
 router.get('/', userController.getAll);
 router.get('/:id', userController.getSingle);
 
+router.post('/', userController.createUser);
+router.get('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
 module.exports = router;
